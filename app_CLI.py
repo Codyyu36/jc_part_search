cd3a16d4c1cdf79ed189900eb84fc27a95ab31d5
codyyu36 <codyyu36@gmail.com>
2024-07-22 23:22:21 -0700

    header

--- app_CLI.py
+++ app_CLI.py
@@ -6,9 +6,14 @@ url = "https://octopart.com/search?q="
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    "Accept-Encoding": "gzip, deflate, br",
     "Accept-Language": "en-US,en;q=0.9",
+    "Connection": "keep-alive",
+    "Upgrade-Insecure-Requests": "1",
     "Referer": "https://www.octopart.com/",
 }
+
 
 def print_info(info_list, table, length):
     #for i in range(0, length):
--- app_test.py
+++ app_test.py
@@ -8,7 +8,11 @@ root_url = "https://octopart.com/search?q="
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    "Accept-Encoding": "gzip, deflate, br",
     "Accept-Language": "en-US,en;q=0.9",
+    "Connection": "keep-alive",
+    "Upgrade-Insecure-Requests": "1",
     "Referer": "https://www.octopart.com/",
 }
 
